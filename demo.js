2a0d1597eee554e6f3774398bb65481d8db2c5f8
swamyNimma <88778775+swamyNimma@users.noreply.github.com>
2021-08-19 13:43:31 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,6 @@
+var test="Hello world"
+var pickData=require('./sendData.js')
+
+console.log("hi :"+pickData.mytext)
+
+console.log("hi swamy and :"+test)
---
+++ sendData.js
@@ -0,0 +1,1 @@
+exports.mytext="testing node"
